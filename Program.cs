39fcdb500326d99c317fad25ae5c47a295abbd6f
Ademodi Seun <seun@zagadat.onmicrosoft.com>
2021-04-29 17:52:34 -0700

    last arr

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace Arrays_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+
+            //  int [] a =  {23,47,89,64,102,90,2,5,10};
+            // //   Array.Sort(a);
+            // //   Array.Reverse(a);
+            //   int sum = 0;
+            //   foreach (var i in a)
+                
+
+            //   {    
+            //       sum += i;
+            //     //    Console.WriteLine(i+5);
+            //        Console.WriteLine(sum);
+                   
+            //   }
+             int [] a =  {23,47,89,64,102,90,2,5,10};
+            Console.WriteLine(a.Sum());
+
+
+        //    int[] Anothernumbers = { 23, 47, 89, 64, 102, 90, 2, 5, 10 };
+        //     Array.Reverse(Anothernumbers);
+        //     int sum = Anothernumbers.Sum();
+        //     Console.WriteLine(sum);
+        //     int sum1 = 0;
+        //     foreach (var item in Anothernumbers)
+        //     {
+        //         sum1 += item;
+        //     }
+        //     Console.WriteLine(sum1);
+
+        }
+    }
+}
